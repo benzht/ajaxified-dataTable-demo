2cf1503b780a21440d5b40d388254ace7eb5b0d8
mansiarora <mansi@intelligrape.com>
2015-08-05 15:06:12 +0530

    Create new domain class 'Employee'; Generated its Controller & CRUD;

---
+++ grails-app/domain/com/ajaxified/datatable/demo/Employee.groovy
@@ -0,0 +1,13 @@
+package com.ajaxified.datatable.demo
+
+class Employee {
+    String firstName
+    String lastName
+    Date dob
+    String office
+    String address
+    BigDecimal salary
+
+    static constraints = {
+    }
+}

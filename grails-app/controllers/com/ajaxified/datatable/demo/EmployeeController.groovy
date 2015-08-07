f8aea03116c2655fb49ebde36644784b2c19dcff
mansiarora <mansi@intelligrape.com>
2015-08-07 17:37:25 +0530

    Added functionality to perform free-text search on the records;

--- grails-app/controllers/com/ajaxified/datatable/demo/EmployeeController.groovy
+++ grails-app/controllers/com/ajaxified/datatable/demo/EmployeeController.groovy
@@ -6,7 +6,7 @@ import groovy.json.JsonSlurper
 
 class EmployeeController {
 
-    private static List<Map>  readAndParseFixture(String fileName) {
+    private static List<Map> readAndParseFixture(String fileName) {
         File jsonFile = Holders.grailsApplication.mainContext.getResource("classpath:" + "${fileName}").file
         return (new JsonSlurper().parseText(jsonFile.text) as Map).data
     }
@@ -17,10 +17,11 @@ class EmployeeController {
 
     /* method used to fetch records through ajax call */
     def ajax_fetchList() {
-        List<Map> employeeList =readAndParseFixture('employees.json')
+        List<Map> employeeList = readAndParseFixture('employees.json')
+        employeeList = searchRecords(employeeList, params)
         Map paginationDetails = getPaginationAndSortDetails(params, employeeList.size())
         employeeList = sortList(employeeList, paginationDetails)
-        List<Map> result = employeeList[paginationDetails.startIndex..paginationDetails.endIndex]
+        List<Map> result = employeeList ? employeeList[paginationDetails.startIndex..paginationDetails.endIndex] : []
         render([data: getList(result), recordsTotal: employeeList.size(), recordsFiltered: employeeList.size()] as JSON)
     }
 
@@ -36,18 +37,27 @@ class EmployeeController {
         return valueList
     }
 
-    private static List<Map> sortList(List<Map> list, Map paginationDetails){
+    private static List<Map> sortList(List<Map> list, Map paginationDetails) {
         if (paginationDetails?.sortOrder) {
             list = list?.sort { it."${paginationDetails.sortBy}" }
             list = (paginationDetails.sortOrder == 'desc') ? list?.reverse() : list
         }
+        return list
+    }
 
+    private static List<Map> searchRecords(List<Map> list, Map parameters) {
+        String searchText = (parameters?."search[value]").toString().trim()
+        if (searchText) {
+            list = list.findAll { Map temp ->
+                temp.any { entry -> entry.value.toString().toLowerCase().contains(searchText.toLowerCase()) }
+            }
+        }
         return list
     }
 
     private static Map getPaginationAndSortDetails(Map parameters, Integer total) {
-        Map order = parameters.findAll { it.key.toString().startsWith('order') } // order[0][dir]:asc where 0 is column no and asc is the sortOrder
-        Integer columnNumber = Integer.parseInt(order.find {it.key.toString().contains('column')}.value.toString()), // fetch column no
+        Map order = parameters.findAll { it.key.toString().startsWith('order') }        // order[0][dir]:asc where 0 is column no and asc is the sortOrder
+        Integer columnNumber = Integer.parseInt(order.find { it.key.toString().contains('column') }.value.toString()), // fetch column no
                 startIndex = Integer.parseInt(parameters.start), endIndex = (startIndex + Integer.parseInt(parameters.length))
         endIndex = endIndex > total ? total : endIndex // check if endIndex is greater than no of records
         String sortOrder = order.find { it.key.toString().contains('dir') }.value// find sort order - asc or desc
